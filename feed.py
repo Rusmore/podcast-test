131b5c327268d43ab9bb2179b79a076ac0458774
Rusmore <129833630+Rusmore@users.noreply.github.com>
2024-09-04 06:54:53 +0000

    Updated

--- feed.py
+++ feed.py
@@ -4,7 +4,7 @@ import xml.etree.ElementTree as xml_tree
 with open('feed.yaml', 'r') as file:
     yaml_data = yaml.safe_load(file)
 
-    rss_element = xml_tree.ElementTree('rss, {'version':'2.0', 
+    rss_element = xml_tree.Element('rss', {'version':'2.0', 
         'xmlns:itunes':'http://www.itunes.com/dtds/podcast-1.0.dtd', 
         'xmlns:content':'http://purl.org/rss/1.0/modules/content/'})
 
@@ -14,4 +14,4 @@ channel_element = xml_tree.SubElement(rss_element, 'channel')
 xml_tree.SubElement(channel_element, 'title').text = yaml_data
 
 output_tree = xml_tree.ElementTree(rss_element)
-output_tree.write('podcast.xml', encoding='UTF-8', xml_declaration=true)
+output_tree.write('podcast.xml', encoding='UTF-8', xml_declaration=True)
